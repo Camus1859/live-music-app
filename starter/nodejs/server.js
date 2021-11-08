99805836c5b39676b77a84e35f4a5ca354e38112
Anderson <laventurepsu1@gmail.com>
2021-11-08 18:21:06 -0500

    modified server for deployement

--- starter/nodejs/server.js
+++ starter/nodejs/server.js
@@ -10,9 +10,17 @@ app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
 
 app.use(express.json({ extended: true }));
 
+const PORT = process.env.PORT || 5000;
+
 app.use('/getArtistSendToCell', getArtistSendToCell);
 
-const PORT = process.env.PORT || 5000;
+if (process.env.NODE_ENV === "production"){
+    app.use(express.static("build"));
+    app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname,  "build", "index.html"));
+    });
+  }
+
 
 app.listen(PORT, () => {
     console.log('server started on port 5000');
